c85a7cf3804aa0d3c6caeb11df711eb8642e6029
Manish Devgan <manish.nsit8@gmail.com>
2017-06-01 01:47:34 +0530

    .js updated
    added support for notes.js

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,45 @@ document.getElementById("overlay").style.display = "block";
 function off() {
 document.getElementById("overlay").style.display = "none";
 }
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+var p;
+var name;
+var div;
+var head;
+var back;
+$('.col-sm-2').click(function(){
+    p = $(this).find('p');
+    //alert(p.text());
+    name = p.text();
+    console.log(name)
+    //alert(p.text());
+    //$('.col-sm-2').css("display","none");
+    $('.col-sm-2').fadeOut(500);
+    sleep(1000);
+    head = document.getElementById("head");
+    //setTimeout(function(){console.log('done');},2000);
+    head.innerHTML = name + " Notes";
+    back = document.getElementById('back')
+    div = document.getElementById(name);
+    //alert(div.innerHTML);
+    sleep(1000);
+    div.style.display = "block";
+    back.style.display = "block";
+});
+
+$('#btn-back').click(function(){
+    back.style.display = "none";
+    div.style.display = "none";
+    sleep(1000);
+    head.innerHTML = "Notes Available to Download!";
+    $('.col-sm-2').fadeIn(500);
+});
